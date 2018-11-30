eae4dd2adee91e9ec5c5763579e2b1a26da7e811
Darlan Rodrigues De Souza <F1811108@for.lanlink.com.br>
2018-11-30 08:41:10 -0300

    Ultimos arquivos da aula  de Expressões Regulares

--- Exemplo11.js
+++ Exemplo11.js
@@ -1,4 +1,4 @@
-// Utilizar as APIs da String para retornar o numero dentro da string.
+// Utiliza os modificadores para retornar todos os telefones presentes na strig  .
 var regExp = /\(\d{2}\)\s?\d{4,5}-?\d{4}/g;
 
 var telefone = "<table><tr><td>(80) 999778899</td><td>(25) 9955-5522</td><td>(22)225553333</td></tr></table>";
